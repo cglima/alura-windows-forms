cef7d7756f240ad35c43572d9ff4163697c0a93d
Cassiana <lima.cglima@gmail.com>
2023-06-20 17:34:57 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class HelloWorld : Form
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
