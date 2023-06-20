ca706a31275057fa835b6ae2c25c5e2d436cebd9
Cassiana <lima.cglima@gmail.com>
2023-06-20 18:00:36 -0300

    feat: Padrões e labels - inserção de um label no form - alteração do nome do form e do label

--- CursoWindowsForms/Frm_HelloWorld.Designer.cs
+++ CursoWindowsForms/Frm_HelloWorld.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CursoWindowsForms
 {
-    partial class HelloWorld
+    partial class Frm_HelloWorld
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,23 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_Titulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // HelloWorld
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Titulo.Location = new System.Drawing.Point(30, 30);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(190, 16);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Visual Studio .NET Version";
+            // 
+            // Frm_HelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "HelloWorld";
+            this.Controls.Add(this.lbl_Titulo);
+            this.Name = "Frm_HelloWorld";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hello World";
             this.Load += new System.EventHandler(this.HelloWorld_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Titulo;
     }
 }
 
