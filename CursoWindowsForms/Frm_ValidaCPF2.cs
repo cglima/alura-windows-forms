b06dc1163fe4ed4f514cb2fe4fc3e457fb7ec110
Cassiana <lima.cglima@gmail.com>
2023-06-21 19:44:56 -0300

    feat: testando o número de dígitos

--- CursoWindowsForms/Frm_ValidaCPF2.cs
+++ CursoWindowsForms/Frm_ValidaCPF2.cs
@@ -31,27 +31,36 @@ namespace CursoWindowsForms
             if(vConteudo == "")
             {
                 MessageBox.Show("Você deve digitar um CPF", "Mensagem de Validação",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                if (MessageBox.Show("Você deseja realmente validar o CPF?",
+                if(vConteudo.Length != 11)
+                {
+                    MessageBox.Show("CPF deve ter 11 digitos", "Mensagem de Validação",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else 
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?",
                                 "Mensagem de Validação", MessageBoxButtons.YesNo,
                                 MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    bool validaCPF = false;
-                    validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
-                    if (validaCPF == true)
                     {
-                        MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    else
-                    {
-                        MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        bool validaCPF = false;
+                        validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
+                        if (validaCPF == true)
+                        {
+                            MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
                 }
+ 
             }
         }
     }
