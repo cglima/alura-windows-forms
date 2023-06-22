4b2dfa17569e460b08c5df20ebc0caf945291370
Cassiana <lima.cglima@gmail.com>
2023-06-22 17:39:02 -0300

    feat: Iniciando o curso 2 da alura de windows forms - criando um projeto de bibliotecas

--- CursoWindowsFormsBiblioteca/Cls_Uteis.cs
+++ CursoWindowsFormsBiblioteca/Cls_Uteis.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CursoWindowsForms
+namespace CursoWindowsFormsBiblioteca
 {
-    internal class Cls_Uteis
+    public class Cls_Uteis
     {
         public static bool Valida(string cpf)
         {
