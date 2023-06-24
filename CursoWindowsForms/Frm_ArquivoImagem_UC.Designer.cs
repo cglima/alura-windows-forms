151c80afbbd973513e84bd1514d2c0fd3992eed2
Cassiana <lima.cglima@gmail.com>
2023-06-24 15:32:02 -0300

    feat: Finalizando o exemplo -criando botão para alterar a cor do label - criando botão para alterar a fonte do label

--- CursoWindowsForms/Frm_ArquivoImagem_UC.Designer.cs
+++ CursoWindowsForms/Frm_ArquivoImagem_UC.Designer.cs
@@ -29,8 +29,10 @@
         private void InitializeComponent()
         {
             this.Lbl_ArquivoImagem = new System.Windows.Forms.Label();
-            this.Píc_ArquivoImagem = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Píc_ArquivoImagem)).BeginInit();
+            this.Pic_ArquivoImagem = new System.Windows.Forms.PictureBox();
+            this.Btn_Cor = new System.Windows.Forms.Button();
+            this.Btn_Fonte = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_ArquivoImagem)).BeginInit();
             this.SuspendLayout();
             // 
             // Lbl_ArquivoImagem
@@ -42,23 +44,45 @@
             this.Lbl_ArquivoImagem.TabIndex = 0;
             this.Lbl_ArquivoImagem.Text = "label1";
             // 
-            // Píc_ArquivoImagem
+            // Pic_ArquivoImagem
             // 
-            this.Píc_ArquivoImagem.Location = new System.Drawing.Point(20, 73);
-            this.Píc_ArquivoImagem.Name = "Píc_ArquivoImagem";
-            this.Píc_ArquivoImagem.Size = new System.Drawing.Size(209, 156);
-            this.Píc_ArquivoImagem.TabIndex = 1;
-            this.Píc_ArquivoImagem.TabStop = false;
+            this.Pic_ArquivoImagem.Location = new System.Drawing.Point(20, 73);
+            this.Pic_ArquivoImagem.Name = "Pic_ArquivoImagem";
+            this.Pic_ArquivoImagem.Size = new System.Drawing.Size(209, 156);
+            this.Pic_ArquivoImagem.TabIndex = 1;
+            this.Pic_ArquivoImagem.TabStop = false;
+            // 
+            // Btn_Cor
+            // 
+            this.Btn_Cor.Location = new System.Drawing.Point(20, 4);
+            this.Btn_Cor.Name = "Btn_Cor";
+            this.Btn_Cor.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Cor.TabIndex = 2;
+            this.Btn_Cor.Text = "Cor";
+            this.Btn_Cor.UseVisualStyleBackColor = true;
+            this.Btn_Cor.Click += new System.EventHandler(this.Btn_Cor_Click);
+            // 
+            // Btn_Fonte
+            // 
+            this.Btn_Fonte.Location = new System.Drawing.Point(101, 4);
+            this.Btn_Fonte.Name = "Btn_Fonte";
+            this.Btn_Fonte.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Fonte.TabIndex = 3;
+            this.Btn_Fonte.Text = "Fonte";
+            this.Btn_Fonte.UseVisualStyleBackColor = true;
+            this.Btn_Fonte.Click += new System.EventHandler(this.Btn_Fonte_Click);
             // 
             // Frm_ArquivoImagem_UC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.Píc_ArquivoImagem);
+            this.Controls.Add(this.Btn_Fonte);
+            this.Controls.Add(this.Btn_Cor);
+            this.Controls.Add(this.Pic_ArquivoImagem);
             this.Controls.Add(this.Lbl_ArquivoImagem);
             this.Name = "Frm_ArquivoImagem_UC";
             this.Size = new System.Drawing.Size(515, 312);
-            ((System.ComponentModel.ISupportInitialize)(this.Píc_ArquivoImagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_ArquivoImagem)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label Lbl_ArquivoImagem;
-        private System.Windows.Forms.PictureBox Píc_ArquivoImagem;
+        private System.Windows.Forms.PictureBox Pic_ArquivoImagem;
+        private System.Windows.Forms.Button Btn_Cor;
+        private System.Windows.Forms.Button Btn_Fonte;
     }
 }
